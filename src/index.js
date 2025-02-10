f6561374d01646b82987eef5bc335362732f42fe
harryhritik <hritikpankal1999@gmail.com>
2025-02-10 16:07:05 +0530

    second

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,4 +13,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
